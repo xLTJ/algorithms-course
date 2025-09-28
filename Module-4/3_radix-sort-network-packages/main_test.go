8c7f05fa3af7ed42b0d5286e4c84e800e5a6f3b6
xLTJ <lucilorate@proton.me>
2025-09-28 18:07:35 +0200

    made test better

--- Module-4/3_radix-sort-network-packages/main_test.go
+++ Module-4/3_radix-sort-network-packages/main_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"slices"
+	"testing"
+)
 
 func Test_radixSortPackages(t *testing.T) {
 	type args struct {
@@ -17,13 +20,35 @@ func Test_radixSortPackages(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			packets, _ := parseInput(tt.args.inputFile)
-			radixSortPackages(packets)
+			packages, _ := parseInput(tt.args.inputFile)
+			originalPackages := slices.Clone(packages) // for comparison later
 
-			prevTimestamp := packets[0].time
-			for i, currentPackage := range packets {
+			radixSortPackages(packages)
+
+			t.Log("Testing if packages got sorted correctly...")
+			prevTimestamp := packages[0].time
+			for i, currentPackage := range packages {
 				if currentPackage.time < prevTimestamp {
 					t.Errorf("package %d has timestamp smaller than previous, packages not correctly sorted :c", i)
+				}
+				prevTimestamp = currentPackage.time
+			}
+
+			t.Log("Packages were sorted correctly, testing if any packages were lost...")
+
+			packageMap := make(map[string]int) // turn it into a map so comparing is faster
+			for _, networkPackage := range packages {
+				packageMap[networkPackage.name] = networkPackage.time
+			}
+
+			for _, originalPackage := range originalPackages {
+				foundPackageTime, ok := packageMap[originalPackage.name]
+				if !ok {
+					t.Errorf("Missing package: %v", originalPackage)
+				}
+
+				if foundPackageTime != originalPackage.time {
+					t.Errorf("Package %s does not match original packet. Got: %v, expected %v", originalPackage.name, foundPackageTime, originalPackage.time)
 				}
 			}
 		})
